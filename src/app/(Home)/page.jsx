4b7f7e801cc1db10dbb6753ec1f2bc55d71bcdea
yesrab <yesrab.ali111@gmail.com>
2024-05-06 11:47:36 +0530

    added suspence elements and removed cache settings

--- src/app/(Home)/page.jsx
+++ src/app/(Home)/page.jsx
@@ -1,11 +1,9 @@
 import Product from "@/components/Product";
-
+import { Suspense } from "react";
 const APIURL = process.env.NEXT_PUBLIC_API;
 
 async function Home() {
-  const response = await fetch(`${APIURL}/api/v1/products/allProducts`, {
-    cache: "no-store",
-  });
+  const response = await fetch(`${APIURL}/api/v1/products/allProducts`);
   const data = await response.json();
 
   return (
@@ -13,7 +11,16 @@ async function Home() {
       <h1 className='text-2xl px-5'>All products !</h1>
       <div className='flex flex-wrap justify-center gap-5 p-3'>
         {data?.allProducts?.map((product) => {
-          return <Product key={product._id} product={product} />;
+          return (
+            <Suspense
+              fallback={
+                <h2 className='flex justify-center shadow-md items-center p-3 w-10 aspect-square'>
+                  Loading...
+                </h2>
+              }>
+              <Product key={product._id} product={product} />
+            </Suspense>
+          );
         })}
       </div>
     </main>
