4225e8eb1a8848e698bf50f73519ca84c038d45d
yesrab <yesrab.ali111@gmail.com>
2024-05-06 11:41:27 +0530

    Revert "combined backend with frontend to eleminate onrender"
    This reverts commit 9437d978ae5de1c2193146ff8d72a3d82c99bce8.

--- server/router/products.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { initProducts, getAllProducts } = require("../controller/products");
-
-router.route("/init").get(initProducts);
-router.route("/allProducts").get(getAllProducts);
-module.exports = router;
